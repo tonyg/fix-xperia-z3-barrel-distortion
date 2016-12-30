8237fd640997bd15430e3901d8a3107a51112e67
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2016-12-31 12:42:58 +1300

    Deal with subdirectories

--- fixer-daemon.py
+++ fixer-daemon.py
@@ -9,25 +9,70 @@ import os
 import subprocess
 import sys
 
+def ensure_dir(p):
+    try:
+        os.makedirs(p)
+    except FileExistsError:
+        return
+    sys.stdout.write('Created dir %s\n' % (p,))
+    sys.stdout.flush()
+
+def clean_dirs(p, stop_at):
+    while not stop_at.startswith(p):
+        try:
+            os.rmdir(p)
+        except OSError:
+            return
+        sys.stdout.write('Removed dir %s\n' % (p,))
+        sys.stdout.flush()
+        p = os.path.dirname(p)
+
 def poll_wonky():
-    for sourcename in glob.glob('Wonky/*'):
-        leafname = os.path.split(sourcename)[1]
-        targetname = os.path.join('Fixed', 'fixed_' + leafname)
-        if not os.path.exists(targetname):
-            sys.stdout.write('Converting %s...' % (leafname,))
+    for (sourcedir, _subdirs, leafnames) in os.walk('Wonky'):
+        subdirparts = sourcedir.split('/')[1:]
+        inprogressdir = os.path.join('Inprogress', *subdirparts)
+        problemsdir = os.path.join('Problems', *subdirparts)
+        targetdir = os.path.join('Fixed', *subdirparts)
+
+        should_clean_dirs = len(subdirparts) and len(leafnames)
+
+        if len(subdirparts): ensure_dir(inprogressdir)
+
+        for leafname in leafnames:
+            sourcefile = os.path.join(sourcedir, leafname)
+            inprogressfile = os.path.join(inprogressdir, 'fixed_' + leafname)
+            targetfile = os.path.join(targetdir, 'fixed_' + leafname)
+            sys.stdout.write('Converting %s...' % (sourcefile,))
             sys.stdout.flush()
-            result = subprocess.run(['convert',
-                                     sourcename,
-                                     '-distort', 'barrel', '0.0132 -0.07765 0.14683',
-                                     targetname])
-            if result.returncode == 0:
+            success = False
+            if os.path.exists(targetfile):
+                result = 'Target file exists'
+            else:
+                result = subprocess.run(['convert',
+                                         sourcefile,
+                                         '-distort', 'barrel', '0.0132 -0.07765 0.14683',
+                                         inprogressfile])
+                if result.returncode == 0:
+                    success = True
+            if success:
                 sys.stdout.write('OK')
-                os.unlink(sourcename)
+                os.unlink(sourcefile)
             else:
                 sys.stdout.write(repr(result))
-                os.rename(sourcename, os.path.join('Problems', leafname))
+                if len(subdirparts): ensure_dir(problemsdir)
+                os.rename(sourcefile, os.path.join(problemsdir, leafname))
             sys.stdout.write('\n')
             sys.stdout.flush()
+        if should_clean_dirs: clean_dirs(sourcedir, 'Wonky')
+
+        if len(subdirparts): ensure_dir(targetdir)
+        for leafname in leafnames:
+            inprogressfile = os.path.join(inprogressdir, 'fixed_' + leafname)
+            targetfile = os.path.join(targetdir, 'fixed_' + leafname)
+            if os.path.exists(inprogressfile):
+                os.rename(inprogressfile, targetfile)
+
+        if should_clean_dirs: clean_dirs(inprogressdir, 'Inprogress')
 
 def main():
     sys.stdout.write('Starting up.\n')
